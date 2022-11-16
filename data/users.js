8702e9177caac37943eb815f3f624b96ed9f92d8
Leon-Masakuna <masakunamfengleon>
2022-11-16 17:52:39 +0100

    feat : users data model

---
+++ data/users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        name: "Leon",
+        password: "1234",
+    },
+    {
+        name: "Eunice",
+        password: "1243",
+    },
+    {
+        name: "Barbine",
+        password: "1423",
+    },
+    {
+        name: "Herman",
+        password: "1324",
+    },
+    {
+        name: "Gloriane",
+        password: "1342",
+    }
+]
+
+module.exports = users;
